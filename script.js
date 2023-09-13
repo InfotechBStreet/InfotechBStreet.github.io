9b38df39b3a1d78fc76f36cfaeed429460ba46c2
WinterSunset95 <wintersunset95@gmail.com>
2023-09-13 16:34:41 +0530

    pushing before switching

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const sidebar = document.getElementById('sidebar');
 
+const scrollToElement = (id) => {
+	const element = document.getElementById(id);
+	element.scrollIntoView({behavior: 'smooth'}, true);
+}
+
 const navToggle = () => {
 	sidebar.classList.toggle('sidebar_active');
 }
